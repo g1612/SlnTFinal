17fd675145d42d1aa42ba227c5a1fe73eb461ac1
Giovana Rosales <givoanarc16@gmail.com>
2022-01-28 03:27:50 +0000

    repositories-sencillos

---
+++ App.TFinal.Repositories/IGeneroRepositories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.TFinal.Models;
+
+namespace App.TFinal.Repositories
+{
+    interface IGeneroRepositories : IRepository<Genero>
+    {
+        Genero BuscarPorId(int id);
+        Task<IEnumerable<Genero>> Listar(string Descripcion);
+        Task<int> Eliminar(int id);
+
+
+    }
+}
